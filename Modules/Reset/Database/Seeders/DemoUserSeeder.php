453518d7f3386104e1e0dff833e18230663ec5d5
Nicolas Widart <n.widart@gmail.com>
2015-06-07 13:43:49 +0200

    Use the dynamic admin role

--- Modules/Reset/Database/Seeders/DemoUserSeeder.php
+++ Modules/Reset/Database/Seeders/DemoUserSeeder.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Seeder;
+use Modules\User\Repositories\RoleRepository;
 use Modules\User\Repositories\UserRepository;
 
 class DemoUserSeeder extends Seeder
@@ -10,21 +11,28 @@ class DemoUserSeeder extends Seeder
      * @var UserRepository
      */
     private $user;
+    /**
+     * @var RoleRepository
+     */
+    private $role;
 
-    public function __construct(UserRepository $user)
+    public function __construct(UserRepository $user, RoleRepository $role)
     {
         $this->user = $user;
+        $this->role = $role;
     }
 
     public function run()
     {
         Model::unguard();
 
+        $role = $this->role->findByName('Admin');
+
         $this->user->createWithRoles([
             'email' => 'john@doe.com',
             'password' => 'demo',
             'first_name' => 'John',
             'last_name' => 'Doe',
-        ], [1], true);
+        ], [$role->id], true);
     }
 }
